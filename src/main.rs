36c086ff09fd56d7c4b1db2046a37d47059b4dc3
RyoLee <516127941@qq.com>
2022-04-14 21:23:28 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 #![windows_subsystem = "windows"]
+extern crate base64;
 
 use base64::{decode, encode};
-use curl::easy::Easy;
-use curl::easy::Form;
+use clipboard::ClipboardContext;
+use clipboard::ClipboardProvider;
 use ini::Ini;
 use md5::compute;
-use clipboard::ClipboardProvider;
-use clipboard::ClipboardContext;
 use std::env;
 use std::io;
 use std::path::PathBuf;
@@ -17,75 +16,48 @@ fn get_cfg_path() -> io::Result<PathBuf> {
     dir.push("xclip.ini");
     Ok(dir)
 }
-fn main() {
+
+fn main() -> Result<(), ureq::Error> {
     let conf = Ini::load_from_file(get_cfg_path().expect("miss config!").to_path_buf()).unwrap();
     let base_url = conf.get_from(Some("main"), "server").unwrap();
-    let key = conf.get_from(Some("main"), "key").unwrap();
+    let id = conf.get_from(Some("main"), "id").unwrap();
     let password = conf.get_from(Some("main"), "password").unwrap();
+    let mut url = base_url.to_string() + &"/".to_string();
+    let id_hash = format!("{:x}", compute(id.as_bytes())).to_string();
+    url += &id_hash;
     let mut args = env::args();
     if args.len() == 2 {
         let mode = args.nth(1).unwrap();
         if mode == "c" {
-            set(key, password, base_url);
-            return;
+            println!("{}", set(&url, password).unwrap());
         }
         if mode == "v" {
-            get(key, password, base_url);
-            return;
+            println!("{}", get(&url, password).unwrap());
         }
     }
+    return Ok(());
 }
-fn get(key: &str, r_password: &str, base_url: &str) {
-    let mut data = Vec::new();
-    let mut handle = Easy::new();
-    let url = base_url.to_string() + "/get";
-    handle.post(true).unwrap();
-    let mut form = Form::new();
-    let password = format!("{:x}", compute(r_password.as_bytes()));
-    form.part("key").contents(key.as_bytes()).add().unwrap();
-    form.part("password").contents(password.as_bytes()).add().unwrap();
-    handle.url(&url.to_string()[..]).unwrap();
-    handle.post(true).unwrap();
-    handle.httppost(form).unwrap();
-    {
-        let mut transfer = handle.transfer();
-        transfer
-            .write_function(|new_data| {
-                data.extend_from_slice(new_data);
-                Ok(new_data.len())
-            })
-            .unwrap();
-        transfer.perform().unwrap();
-    }
-    let db = decode(data).unwrap();
+fn get(url: &str, password: &str) -> Result<String, ureq::Error> {
+    let password_hash = format!("{:x}", compute(password.as_bytes()));
+    let body: String = ureq::get(url)
+        .set("password", &password_hash)
+        .call()?
+        .into_string()?;
+    let db = decode(&body.to_string()).unwrap();
     let res = String::from_utf8_lossy(&db);
+    println!("{}", res.to_string());
     let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
     ctx.set_contents(res.to_string()).unwrap();
+    return Ok(res.to_string());
 }
-fn set(key: &str, r_password: &str, base_url: &str) {
-    let mut data = Vec::new();
-    let mut handle = Easy::new();
-    let url = base_url.to_string() + "/set";
-    handle.post(true).unwrap();
-    let mut form = Form::new();
+fn set(url: &str, password: &str) -> Result<String, ureq::Error> {
+    let password_hash = format!("{:x}", compute(password.as_bytes()));
     let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
     let raw_value = ctx.get_contents().unwrap();
     let value = encode(raw_value);
-    let password = format!("{:x}", compute(r_password.as_bytes()));
-    form.part("key").contents(key.as_bytes()).add().unwrap();
-    form.part("password").contents(password.as_bytes()).add().unwrap();
-    form.part("value").contents(value.as_bytes()).add().unwrap();
-    handle.url(&url.to_string()[..]).unwrap();
-    handle.post(true).unwrap();
-    handle.httppost(form).unwrap();
-    {
-        let mut transfer = handle.transfer();
-        transfer
-            .write_function(|new_data| {
-                data.extend_from_slice(new_data);
-                Ok(new_data.len())
-            })
-            .unwrap();
-        transfer.perform().unwrap();
-    }
+    let body: String = ureq::post(url)
+        .set("password", &password_hash)
+        .send_form(&[("value", &value)])?
+        .into_string()?;
+    return Ok(body);
 }
--- src/xclip.ahk
+++ src/xclip.ahk
@@ -1,8 +1,8 @@
 SC029 & C:: {
-	Run("xclip.exe c")
+    Run("xclip.exe c")
     send("^c")
 }
 SC029 & V:: {
-	Run("xclip.exe v")
+    Run("xclip.exe v")
     send("^v")
 }
